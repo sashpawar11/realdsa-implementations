8d0a2f863ea4dd69e68b9669dc8089fdd96532de
sashpawar11 <saisaipawar@gmail.com>
2025-12-23 10:20:34 +0530

    weighted graphs reprs

--- graphs/adjacency-list.cpp
+++ graphs/adjacency-list.cpp
@@ -26,3 +26,31 @@ int main() {
 
   return 0;
 }
+
+
+// for Weighted Graphs
+int weightedGraphsAdjList() {
+
+  // Adjacency List Representation for Weighted Graphs
+  int n,m;              // Inputs : n = no. nodes, m = no. edges
+  cin >> n >> m;
+
+  vector<pair<int,int>> adjlist[n+1];        // for 1-based indexed graph. For 0-based indexed graph, use adjlist[n]
+
+  for(int i = 0; i<m; i++){
+        int u, v, w;          // edge between node u and v will be provided on each line in the edges matrix; w = weight of edge
+        cin >> u >> v >> w;
+
+        pair<int,int> node1 = {v,w};
+        pair<int,int> node2 = {u,w};
+
+        // Store neigbhous at indexes of adj list; where index has the pairs of (neighbour node, weight of edge) items
+        adjlist[u].push_back(node1);
+        adjlist[v].push_back(node2);
+
+        // for directed graph, we would not have line 50, as edge between u-v is the only edge.
+  }
+
+
+  return 0;
+}
--- graphs/adjacency-matrix.cpp
+++ graphs/adjacency-matrix.cpp
@@ -22,3 +22,27 @@ int main() {
 
   return 0;
 }
+
+
+// for Weighted Graphs
+int weightedGraphsAdjMatrix() {
+
+  // Adjacency Matrix Representation for Weighted Graphs
+  int n,m;              // Inputs : n = no. nodes, m = no. edges
+  cin >> n >> m;
+
+  int adj[n+1][m+1];        // if 1-based indexing graph.   -> if 0-based indexing graph, take n X m : i.e. adj[n][m]
+
+  for(int i = 0; i<m; i++){
+        int u, v, w;          // edge between node u and v will be provided on each line in the edges matrix
+        cin >> u >> v >> w;
+
+        // for undirected graph --> mark edge and opposite edge (bi-directional) as 1
+        adj[u][v] = w;
+        adj[v][u] = w;
+        // for directed graph, we would not have line 42, as edge between u-v is the only edge.
+  }
+
+
+  return 0;
+}
